82bdb3599319d75de1470fbe4ba17e9eb0916430
Andreas Eulitz <aeulitz@microsoft.com>
2020-07-30 18:20:02 -0700

    rename test pattern methods, add test for 'void(void)' method

--- FooPattern.h
+++ FooPattern.h
@@ -8,11 +8,11 @@ namespace App
 	//__declspec(uuid("c29e6e8c-47e5-4405-95a8-706d4d0d95b3"))
 	struct FooPattern : public CustomPatternBase<FooPattern>
 	{
-		//CUSTOM_PATTERN_METHOD(0, GetFoo, "e6b31052-4a7e-4dad-b341-855609f42232")
+		//CUSTOM_PATTERN_METHOD(0, GetString, "e6b31052-4a7e-4dad-b341-855609f42232")
 		template<class TPattern, class TRegistrar>
 		static constexpr void RegisterMethod(std::integral_constant<int, 0> n)
 		{
-			TRegistrar::Register(Guid::StringToGuid("e6b31052-4a7e-4dad-b341-855609f42232"), &TPattern::GetFoo);
+			TRegistrar::Register(Guid::StringToGuid("e6b31052-4a7e-4dad-b341-855609f42232"), &TPattern::GetString);
 			if constexpr (n > 0) RegisterMethod<TPattern, TRegistrar>(std::integral_constant<int, n - 1>{});
 		}
 		template<class TRegistrar>
@@ -21,17 +21,17 @@ namespace App
 			TRegistrar::Unregister(Guid::StringToGuid("e6b31052-4a7e-4dad-b341-855609f42232"));
 			if constexpr (n > 0) UnregisterMethod<TRegistrar>(std::integral_constant<int, n - 1>{});
 		}
-		const wchar_t* GetFoo()
+		const wchar_t* GetString()
 		{
-			if (OnGetFoo) OnGetFoo();
-			return m_foo.c_str();
+			if (OnGetString) OnGetString();
+			return m_string.c_str();
 		}
 
 		// CUSTOM_PATTERN_METHOD(1, SetFoo, "4426d571-240c-47bc-8d5a-51f2974b4beb")
 		template<class TPattern, class TRegistrar>
 		static constexpr void RegisterMethod(std::integral_constant<int, 1> n)
 		{
-			TRegistrar::Register(Guid::StringToGuid("4426d571-240c-47bc-8d5a-51f2974b4beb"), &TPattern::SetFoo);
+			TRegistrar::Register(Guid::StringToGuid("4426d571-240c-47bc-8d5a-51f2974b4beb"), &TPattern::SetString);
 			if constexpr (n > 0) RegisterMethod<TPattern, TRegistrar>(std::integral_constant<int, n - 1>{});
 		}
 		template<class TRegistrar>
@@ -40,32 +40,34 @@ namespace App
 			TRegistrar::Unregister(Guid::StringToGuid("4426d571-240c-47bc-8d5a-51f2974b4beb"));
 			if constexpr (n > 0) UnregisterMethod<TRegistrar>(std::integral_constant<int, n - 1>{});
 		}
-		void SetFoo(const std::wstring& val)
+		void SetString(const std::wstring& val)
 		{
-			if (OnSetFoo) OnSetFoo(val);
-			m_foo = val;
+			if (OnSetString) OnSetString(val);
+			m_string = val;
 		}
 
-		CUSTOM_PATTERN_METHOD(2, AppendFoo, "c583a435-e0c1-4ba9-be2d-6b8d714c7918")
-		void AppendFoo(const std::wstring& val)
+		CUSTOM_PATTERN_METHOD(2, AppendString, "c583a435-e0c1-4ba9-be2d-6b8d714c7918")
+		void AppendString(const std::wstring& val)
 		{
-			m_foo += val;
+			m_string += val;
 		}
 
-		CUSTOM_PATTERN_METHOD(3, ClearFoo, "13e7a41d-bad4-4ab2-806b-e57128302684")
-		void ClearFoo()
+		CUSTOM_PATTERN_METHOD(3, ClearString, "13e7a41d-bad4-4ab2-806b-e57128302684")
+		void ClearString()
 		{
-			m_foo.clear();
+			if (OnClearString) OnClearString();
+			m_string.clear();
 		}
 
 		//
 		// for testing
 		//
 
-		std::function<void()> OnGetFoo = nullptr;
-		std::function<void(const std::wstring&)> OnSetFoo = nullptr;
+		std::function<void()> OnGetString = nullptr;
+		std::function<void(const std::wstring&)> OnSetString = nullptr;
+		std::function<void()> OnClearString = nullptr;
 
 	private:
-		std::wstring m_foo;
+		std::wstring m_string;
 	};
 }
--- Tests.cpp
+++ Tests.cpp
@@ -57,27 +57,27 @@ namespace UnitTest1
 			Assert::AreEqual(GUID{ 0xe6b31052, 0x4a7e, 0x4dad, {0xb3, 0x41, 0x85, 0x56, 0x09, 0xf4, 0x22, 0x32} }, guids[3]);
 		}
 
-		TEST_METHOD(GetFoo)
+		TEST_METHOD(GetString)
 		{
 			App::FooPattern::RegisterMethods();
 			Assert::AreEqual(4ull, Microsoft::UIA::TestOnly_RemoteOperationCount());
 
 			auto fooPatternInstance = winrt::make<App::FooPattern>();
-			int getFooCallCount = 0;
-			fooPatternInstance.as<App::FooPattern>()->OnGetFoo = [&getFooCallCount]() { ++getFooCallCount; };
+			int getStringCallCount = 0;
+			fooPatternInstance.as<App::FooPattern>()->OnGetString = [&getStringCallCount]() { ++getStringCallCount; };
 
 			Microsoft::UIA::RemoteOperationContext context;
 			context.SetOperand(0, fooPatternInstance);
 
-			Microsoft::UIA::CallRemoteOperationExtension(getFooGuid, context, { 0 });
+			Microsoft::UIA::CallRemoteOperationExtension(getStringGuid, context, { 0 });
 
-			Assert::AreEqual(1, getFooCallCount);
+			Assert::AreEqual(1, getStringCallCount);
 
 			App::FooPattern::UnregisterMethods();
 			Assert::AreEqual(0ull, Microsoft::UIA::TestOnly_RemoteOperationCount());
 		}
 
-		TEST_METHOD(SetFoo)
+		TEST_METHOD(SetString)
 		{
 			App::FooPattern::RegisterMethods();
 			Assert::AreEqual(4ull, Microsoft::UIA::TestOnly_RemoteOperationCount());
@@ -85,25 +85,51 @@ namespace UnitTest1
 			auto fooPatternInstance = winrt::make<App::FooPattern>();
 
 			std::vector<std::wstring> arguments;
-			fooPatternInstance.as<App::FooPattern>()->OnSetFoo = [&arguments](const std::wstring& val) { arguments.push_back(val); };
+			fooPatternInstance.as<App::FooPattern>()->OnSetString = [&arguments](const std::wstring& val) { arguments.push_back(val); };
 
 			Microsoft::UIA::RemoteOperationContext context;
 			context.SetOperand(0, fooPatternInstance);
 			context.SetOperand(1, winrt::box_value(L"string cheese"));
 
-			Microsoft::UIA::CallRemoteOperationExtension(setFooGuid, context, { 0, 1 });
+			Microsoft::UIA::CallRemoteOperationExtension(setStringGuid, context, { 0, 1 });
 
 			Assert::AreEqual(1ull, arguments.size());
 			Assert::AreEqual(L"string cheese", arguments[0].c_str());
 
-			Assert::AreEqual(L"string cheese", fooPatternInstance.as<App::FooPattern>()->GetFoo());
+			Assert::AreEqual(L"string cheese", fooPatternInstance.as<App::FooPattern>()->GetString());
+
+			App::FooPattern::UnregisterMethods();
+			Assert::AreEqual(0ull, Microsoft::UIA::TestOnly_RemoteOperationCount());
+		}
+
+		TEST_METHOD(ClearString)
+		{
+			App::FooPattern::RegisterMethods();
+			Assert::AreEqual(4ull, Microsoft::UIA::TestOnly_RemoteOperationCount());
+
+			auto fooPatternInstance = winrt::make<App::FooPattern>();
+
+			// set the value to be able to verify the effect of the 'ClearString' call
+			fooPatternInstance.as<App::FooPattern>()->SetString(L"meatballs");
+
+			int clearStringCallCount = 0;
+			fooPatternInstance.as<App::FooPattern>()->OnClearString = [&clearStringCallCount]() { ++clearStringCallCount; };
+
+			Microsoft::UIA::RemoteOperationContext context;
+			context.SetOperand(0, fooPatternInstance);
+
+			Microsoft::UIA::CallRemoteOperationExtension(clearStringGuid, context, { 0 });
+
+			Assert::AreEqual(1, clearStringCallCount);
+			Assert::AreEqual(L"", fooPatternInstance.as<App::FooPattern>()->GetString());
 
 			App::FooPattern::UnregisterMethods();
 			Assert::AreEqual(0ull, Microsoft::UIA::TestOnly_RemoteOperationCount());
 		}
 
 	private:
-		static inline GUID getFooGuid{ 0xe6b31052, 0x4a7e, 0x4dad, {0xb3, 0x41, 0x85, 0x56, 0x09, 0xf4, 0x22, 0x32} };
-		static inline GUID setFooGuid{ 0x4426d571, 0x240c, 0x47bc, {0x8d, 0x5a, 0x51, 0xf2, 0x97, 0x4b, 0x4b, 0xeb} };
+		static inline GUID getStringGuid{ 0xe6b31052, 0x4a7e, 0x4dad, {0xb3, 0x41, 0x85, 0x56, 0x09, 0xf4, 0x22, 0x32} };
+		static inline GUID setStringGuid{ 0x4426d571, 0x240c, 0x47bc, {0x8d, 0x5a, 0x51, 0xf2, 0x97, 0x4b, 0x4b, 0xeb} };
+		static inline GUID clearStringGuid{ 0x13e7a41d, 0xbad4, 0x4ab2, {0x80, 0x6b, 0xe5, 0x71, 0x28, 0x30, 0x26, 0x84} };
 	};
 }
