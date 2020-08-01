a9c967a901ef507995895deaf301c6b06cd0b13e
Andreas Eulitz <aeulitz@microsoft.com>
2020-07-31 19:58:14 -0700

    mockify FooPattern

--- FooPattern.h
+++ FooPattern.h
@@ -10,110 +10,92 @@ namespace App
 		void SetBool(bool val)
 		{
 			// example of a 'void(bool)' method
-			if (OnSetBool) OnSetBool(val);
-			m_bool = val;
+			OnSetBool(val);
 		}
 
 		CUSTOM_PATTERN_METHOD(1, GetBool, "a9487d87-8935-49ad-9473-fadce0bfa974")
 		bool GetBool()
 		{
 			// example of a 'bool(void)' method
-			if (OnGetBool) OnGetBool();
-			return m_bool;
+			return OnGetBool();
 		}
 
 		CUSTOM_PATTERN_METHOD(2, SetInt, "921a5d67-9a8f-4c38-b676-5c8c2d44ef18")
 		void SetInt(int val)
 		{
 			// example of a 'void(int)' method
-			if (OnSetInt) OnSetInt(val);
-			m_int = val;
+			OnSetInt(val);
 		}
 
 		CUSTOM_PATTERN_METHOD(3, GetInt, "85e4d90e-a804-4a45-a0e2-3f572f5ccfa9")
 		int GetInt()
 		{
 			// example of an 'int(void)' method
-			if (OnGetInt) OnGetInt();
-			return m_int;
+			return OnGetInt();
 		}
 
 		CUSTOM_PATTERN_METHOD(4, SetFloat, "4d111a79-4b56-4a23-aefa-a56b51337f09")
 		void SetFloat(float val)
 		{
 			// example of a 'void(float)' method
-			if (OnSetFloat) OnSetFloat(val);
-			m_float = val;
+			OnSetFloat(val);
 		}
 
 		CUSTOM_PATTERN_METHOD(5, GetFloat, "71667878-2e43-4901-af9c-0eb139f79439")
 		float GetFloat()
 		{
 			// example of a 'float(void)' method
-			if (OnGetFloat) OnGetFloat();
-			return m_float;
+			return OnGetFloat();
 		}
 
 		CUSTOM_PATTERN_METHOD(6, SetDouble, "16c06415-52f3-4317-8ffc-a2d4b39cddfb")
 		void SetDouble(double val)
 		{
 			// example of a 'void(double)' method
-			if (OnSetDouble) OnSetDouble(val);
-			m_double = val;
+			OnSetDouble(val);
 		}
 
 		CUSTOM_PATTERN_METHOD(7, GetDouble, "643bd34c-c22c-4255-92e7-e2ed4a104782")
 		double GetDouble()
 		{
 			// example of a 'double(void)' method
-			if (OnGetDouble) OnGetDouble();
-			return m_double;
+			return OnGetDouble();
 		}
 
 		CUSTOM_PATTERN_METHOD(8, SetString, "4426d571-240c-47bc-8d5a-51f2974b4beb")
 		void SetString(const std::wstring& val)
 		{
 			// example of a 'void(const std::wstring&)' method
-			if (OnSetString) OnSetString(val);
-			m_string = val;
+			OnSetString(val);
 		}
 
 		CUSTOM_PATTERN_METHOD(9, GetString, "e6b31052-4a7e-4dad-b341-855609f42232")
 		const wchar_t* GetString()
 		{
 			// example of a 'const wchar_t*()' method
-			if (OnGetString) OnGetString();
-			return m_string.c_str();
+			return OnGetString();
 		}
 
 		CUSTOM_PATTERN_METHOD(10, ClearString, "13e7a41d-bad4-4ab2-806b-e57128302684")
 		void ClearString()
 		{
 			// example of a 'void(void)' method
-			if (OnClearString) OnClearString();
-			m_string.clear();
+			OnClearString();
 		}
 
 		//
 		// for testing
 		//
 		std::function<void(bool)> OnSetBool = nullptr;
-		std::function<void()> OnGetBool = nullptr;
+		std::function<bool()> OnGetBool = nullptr;
 		std::function<void(int)> OnSetInt = nullptr;
-		std::function<void()> OnGetInt = nullptr;
+		std::function<int()> OnGetInt = nullptr;
 		std::function<void(float)> OnSetFloat = nullptr;
-		std::function<void()> OnGetFloat = nullptr;
+		std::function<float()> OnGetFloat = nullptr;
 		std::function<void(double)> OnSetDouble = nullptr;
-		std::function<void()> OnGetDouble = nullptr;
+		std::function<double()> OnGetDouble = nullptr;
 		std::function<void(const std::wstring&)> OnSetString = nullptr;
-		std::function<void()> OnGetString = nullptr;
+		std::function<const wchar_t*()> OnGetString = nullptr;
 		std::function<void()> OnClearString = nullptr;
-
-	private:
-		bool m_bool = false;
-		int m_int = 0;
-		float m_float = 0.0f;
-		double m_double = 0.0;
-		std::wstring m_string;
 	};
 }
--- Tests.cpp
+++ Tests.cpp
@@ -71,17 +71,67 @@ namespace CuspTest
 			Assert::AreEqual(setBoolGuid, guids[10]);
 		}
 
+		TEST_METHOD(GetBool)
+		{
+			App::FooPattern::RegisterMethods();
+
+			auto fooPatternInstance = winrt::make<App::FooPattern>();
+
+			int getBoolCallCount = 0;
+			fooPatternInstance.as<App::FooPattern>()->OnGetBool = [&getBoolCallCount]()
+			{
+				++getBoolCallCount;
+				return true;
+			};
+
+			Microsoft::UIA::RemoteOperationContext context;
+			context.SetOperand(0, fooPatternInstance);
+
+			Microsoft::UIA::CallRemoteOperationExtension(getBoolGuid, context, { 0, 1 });
+
+			Assert::AreEqual(1, getBoolCallCount);
+			Assert::IsTrue(winrt::unbox_value<bool>(context.GetOperand(1)));
+
+			App::FooPattern::UnregisterMethods();
+		}
+
+		TEST_METHOD(SetBool)
+		{
+			App::FooPattern::RegisterMethods();
+
+			auto fooPatternInstance = winrt::make<App::FooPattern>();
+
+			std::vector<bool> setBoolArguments;
+			fooPatternInstance.as<App::FooPattern>()->OnSetBool =
+				[&setBoolArguments](bool val)
+			{
+				setBoolArguments.push_back(val);
+			};
+
+			Microsoft::UIA::RemoteOperationContext context;
+			context.SetOperand(0, fooPatternInstance);
+			context.SetOperand(1, winrt::box_value(true));
+
+			Microsoft::UIA::CallRemoteOperationExtension(setBoolGuid, context, { 0, 1 });
+
+			Assert::AreEqual(1ull, setBoolArguments.size());
+
+			App::FooPattern::UnregisterMethods();
+		}
+
 		TEST_METHOD(GetInt)
 		{
 			App::FooPattern::RegisterMethods();
 
 			auto fooPatternInstance = winrt::make<App::FooPattern>();
 
-			// set the value to be able to verify the return value of the 'GetInt' call
-			fooPatternInstance.as<App::FooPattern>()->SetInt(23);
-
 			int getIntCallCount = 0;
-			fooPatternInstance.as<App::FooPattern>()->OnGetInt = [&getIntCallCount]() { ++getIntCallCount; };
+			fooPatternInstance.as<App::FooPattern>()->OnGetInt =
+				[&getIntCallCount]()
+				{
+					++getIntCallCount;
+					return 23;
+				};
 
 			Microsoft::UIA::RemoteOperationContext context;
 			context.SetOperand(0, fooPatternInstance);
@@ -102,7 +152,12 @@ namespace CuspTest
 			auto fooPatternInstance = winrt::make<App::FooPattern>();
 
 			std::vector<int> setIntArguments;
-			fooPatternInstance.as<App::FooPattern>()->OnSetInt = [&setIntArguments](int val) { setIntArguments.push_back(val); };
+			fooPatternInstance.as<App::FooPattern>()->OnSetInt =
+				[&setIntArguments]
+				(int val)
+				{
+					setIntArguments.push_back(val);
+				};
 
 			Microsoft::UIA::RemoteOperationContext context;
 			context.SetOperand(0, fooPatternInstance);
@@ -113,32 +168,6 @@ namespace CuspTest
 			Assert::AreEqual(1ull, setIntArguments.size());
 			Assert::AreEqual(23, setIntArguments[0]);
 
-			Assert::AreEqual(23, fooPatternInstance.as<App::FooPattern>()->GetInt());
-
-			App::FooPattern::UnregisterMethods();
-		}
-
-		TEST_METHOD(GetBool)
-		{
-			App::FooPattern::RegisterMethods();
-
-			auto fooPatternInstance = winrt::make<App::FooPattern>();
-
-			// set the value to be able to verify the return value of the 'GetBool' call
-			fooPatternInstance.as<App::FooPattern>()->SetBool(true);
-
-			int getBoolCallCount = 0;
-			fooPatternInstance.as<App::FooPattern>()->OnGetBool = [&getBoolCallCount]() { ++getBoolCallCount; };
-
-			Microsoft::UIA::RemoteOperationContext context;
-			context.SetOperand(0, fooPatternInstance);
-
-			Microsoft::UIA::CallRemoteOperationExtension(getBoolGuid, context, { 0, 1 });
-
-			Assert::AreEqual(1, getBoolCallCount);
-
-			Assert::IsTrue(winrt::unbox_value<bool>(context.GetOperand(1)));
-
 			App::FooPattern::UnregisterMethods();
 		}
 
@@ -148,11 +177,13 @@ namespace CuspTest
 
 			auto fooPatternInstance = winrt::make<App::FooPattern>();
 
-			// set the value to be able to verify the return value of the 'GetFloat' call
-			fooPatternInstance.as<App::FooPattern>()->SetFloat(1.234f);
-
 			int getFloatCallCount = 0;
-			fooPatternInstance.as<App::FooPattern>()->OnGetFloat = [&getFloatCallCount]() { ++getFloatCallCount; };
+			fooPatternInstance.as<App::FooPattern>()->OnGetFloat =
+				[&getFloatCallCount]()
+				{
+					++getFloatCallCount;
+					return 1.234f;
+				};
 
 			Microsoft::UIA::RemoteOperationContext context;
 			context.SetOperand(0, fooPatternInstance);
@@ -160,7 +191,6 @@ namespace CuspTest
 			Microsoft::UIA::CallRemoteOperationExtension(getFloatGuid, context, { 0, 1 });
 
 			Assert::AreEqual(1, getFloatCallCount);
-
 			Assert::AreEqual(1.234f, winrt::unbox_value<float>(context.GetOperand(1)));
 
 			App::FooPattern::UnregisterMethods();
@@ -184,8 +214,6 @@ namespace CuspTest
 			Assert::AreEqual(1ull, setFloatArguments.size());
 			Assert::AreEqual(1.234f, setFloatArguments[0]);
 
-			Assert::AreEqual(1.234f, fooPatternInstance.as<App::FooPattern>()->GetFloat());
-
 			App::FooPattern::UnregisterMethods();
 		}
 
@@ -195,11 +223,13 @@ namespace CuspTest
 
 			auto fooPatternInstance = winrt::make<App::FooPattern>();
 
-			// set the value to be able to verify the return value of the 'GetDouble' call
-			fooPatternInstance.as<App::FooPattern>()->SetDouble(1.234);
-
 			int getDoubleCallCount = 0;
-			fooPatternInstance.as<App::FooPattern>()->OnGetDouble = [&getDoubleCallCount]() { ++getDoubleCallCount; };
+			fooPatternInstance.as<App::FooPattern>()->OnGetDouble =
+				[&getDoubleCallCount]()
+				{
+					++getDoubleCallCount;
+					return 1.234;
+				};
 
 			Microsoft::UIA::RemoteOperationContext context;
 			context.SetOperand(0, fooPatternInstance);
@@ -207,7 +237,6 @@ namespace CuspTest
 			Microsoft::UIA::CallRemoteOperationExtension(getDoubleGuid, context, { 0, 1 });
 
 			Assert::AreEqual(1, getDoubleCallCount);
-
 			Assert::AreEqual(1.234, winrt::unbox_value<double>(context.GetOperand(1)));
 
 			App::FooPattern::UnregisterMethods();
@@ -220,7 +249,12 @@ namespace CuspTest
 			auto fooPatternInstance = winrt::make<App::FooPattern>();
 
 			std::vector<double> setDoubleArguments;
-			fooPatternInstance.as<App::FooPattern>()->OnSetDouble = [&setDoubleArguments](double val) { setDoubleArguments.push_back(val); };
+			fooPatternInstance.as<App::FooPattern>()->OnSetDouble =
+				[&setDoubleArguments]
+				(double val)
+				{
+					setDoubleArguments.push_back(val);
+				};
 
 			Microsoft::UIA::RemoteOperationContext context;
 			context.SetOperand(0, fooPatternInstance);
@@ -230,31 +264,6 @@ namespace CuspTest
 
 			Assert::AreEqual(1ull, setDoubleArguments.size());
 			Assert::AreEqual(1.234, setDoubleArguments[0]);
-
-			Assert::AreEqual(1.234, fooPatternInstance.as<App::FooPattern>()->GetDouble());
-
-			App::FooPattern::UnregisterMethods();
-		}
-
-		TEST_METHOD(SetString)
-		{
-			App::FooPattern::RegisterMethods();
-
-			auto fooPatternInstance = winrt::make<App::FooPattern>();
-
-			std::vector<std::wstring> setStringArguments;
-			fooPatternInstance.as<App::FooPattern>()->OnSetString = [&setStringArguments](const std::wstring& val) { setStringArguments.push_back(val); };
-
-			Microsoft::UIA::RemoteOperationContext context;
-			context.SetOperand(0, fooPatternInstance);
-			context.SetOperand(1, winrt::box_value(L"string cheese"));
-
-			Microsoft::UIA::CallRemoteOperationExtension(setStringGuid, context, { 0, 1 });
-
-			Assert::AreEqual(1ull, setStringArguments.size());
-			Assert::AreEqual(L"string cheese", setStringArguments[0].c_str());
-
-			Assert::AreEqual(L"string cheese", fooPatternInstance.as<App::FooPattern>()->GetString());
 
 			App::FooPattern::UnregisterMethods();
 		}
@@ -266,11 +275,13 @@ namespace CuspTest
 
 			auto fooPatternInstance = winrt::make<App::FooPattern>();
 
-			// set the value to be able to verify the return value of the 'GetString' call
-			fooPatternInstance.as<App::FooPattern>()->SetString(L"pool noodle");
-
 			int getStringCallCount = 0;
-			fooPatternInstance.as<App::FooPattern>()->OnGetString = [&getStringCallCount]() { ++getStringCallCount; };
+			fooPatternInstance.as<App::FooPattern>()->OnGetString =
+				[&getStringCallCount]()
+				{
+					++getStringCallCount;
+					return L"pool noodle";
+				};
 
 			Microsoft::UIA::RemoteOperationContext context;
 			context.SetOperand(0, fooPatternInstance);
@@ -285,14 +296,37 @@ namespace CuspTest
 			Assert::AreEqual(0ull, Microsoft::UIA::TestOnly_RemoteOperationCount());
 		}
 
-		TEST_METHOD(ClearString)
+		TEST_METHOD(SetString)
 		{
 			App::FooPattern::RegisterMethods();
 
 			auto fooPatternInstance = winrt::make<App::FooPattern>();
 
-			// set the value to be able to verify the effect of the 'ClearString' call
-			fooPatternInstance.as<App::FooPattern>()->SetString(L"meatballs");
+			std::vector<std::wstring> setStringArguments;
+			fooPatternInstance.as<App::FooPattern>()->OnSetString =
+				[&setStringArguments]
+				(const std::wstring& val)
+				{
+					setStringArguments.push_back(val);
+				};
+
+			Microsoft::UIA::RemoteOperationContext context;
+			context.SetOperand(0, fooPatternInstance);
+			context.SetOperand(1, winrt::box_value(L"string cheese"));
+
+			Microsoft::UIA::CallRemoteOperationExtension(setStringGuid, context, { 0, 1 });
+
+			Assert::AreEqual(1ull, setStringArguments.size());
+			Assert::AreEqual(L"string cheese", setStringArguments[0].c_str());
+
+			App::FooPattern::UnregisterMethods();
+		}
+
+		TEST_METHOD(ClearString)
+		{
+			App::FooPattern::RegisterMethods();
+
+			auto fooPatternInstance = winrt::make<App::FooPattern>();
 
 			int clearStringCallCount = 0;
 			fooPatternInstance.as<App::FooPattern>()->OnClearString = [&clearStringCallCount]() { ++clearStringCallCount; };
@@ -303,20 +337,19 @@ namespace CuspTest
 			Microsoft::UIA::CallRemoteOperationExtension(clearStringGuid, context, { 0 });
 
 			Assert::AreEqual(1, clearStringCallCount);
-			Assert::AreEqual(L"", fooPatternInstance.as<App::FooPattern>()->GetString());
 
 			App::FooPattern::UnregisterMethods();
 		}
 
 	private:
-		static inline GUID setBoolGuid{ 0x25fb1199, 0xdb6f, 0x4349, {0x86, 0xaa, 0x43, 0x6d, 0x37, 0x6a, 0x68, 0x43} };
 		static inline GUID getBoolGuid{ 0xa9487d87, 0x8935, 0x49ad, {0x94, 0x73, 0xfa, 0xdc, 0xe0, 0xbf, 0xa9, 0x74} };
-		static inline GUID setIntGuid{ 0x921a5d67, 0x9a8f, 0x4c38, {0xb6, 0x76, 0x5c, 0x8c, 0x2d, 0x44, 0xef, 0x18} };
+		static inline GUID setBoolGuid{ 0x25fb1199, 0xdb6f, 0x4349, {0x86, 0xaa, 0x43, 0x6d, 0x37, 0x6a, 0x68, 0x43} };
 		static inline GUID getIntGuid{ 0x85e4d90e, 0xa804, 0x4a45, {0xa0, 0xe2, 0x3f, 0x57, 0x2f, 0x5c, 0xcf, 0xa9} };
-		static inline GUID setFloatGuid{ 0x4d111a79, 0x4b56, 0x4a23, {0xae, 0xfa, 0xa5, 0x6b, 0x51, 0x33, 0x7f, 0x09} };
+		static inline GUID setIntGuid{ 0x921a5d67, 0x9a8f, 0x4c38, {0xb6, 0x76, 0x5c, 0x8c, 0x2d, 0x44, 0xef, 0x18} };
 		static inline GUID getFloatGuid{ 0x71667878, 0x2e43, 0x4901, {0xaf, 0x9c, 0x0e, 0xb1, 0x39, 0xf7, 0x94, 0x39} };
-		static inline GUID setDoubleGuid{ 0x16c06415, 0x52f3, 0x4317, {0x8f, 0xfc, 0xa2, 0xd4, 0xb3, 0x9c, 0xdd, 0xfb} };
+		static inline GUID setFloatGuid{ 0x4d111a79, 0x4b56, 0x4a23, {0xae, 0xfa, 0xa5, 0x6b, 0x51, 0x33, 0x7f, 0x09} };
 		static inline GUID getDoubleGuid{ 0x643bd34c, 0xc22c, 0x4255, {0x92, 0xe7, 0xe2, 0xed, 0x4a, 0x10, 0x47, 0x82} };
+		static inline GUID setDoubleGuid{ 0x16c06415, 0x52f3, 0x4317, {0x8f, 0xfc, 0xa2, 0xd4, 0xb3, 0x9c, 0xdd, 0xfb} };
 		static inline GUID getStringGuid{ 0xe6b31052, 0x4a7e, 0x4dad, {0xb3, 0x41, 0x85, 0x56, 0x09, 0xf4, 0x22, 0x32} };
 		static inline GUID setStringGuid{ 0x4426d571, 0x240c, 0x47bc, {0x8d, 0x5a, 0x51, 0xf2, 0x97, 0x4b, 0x4b, 0xeb} };
 		static inline GUID clearStringGuid{ 0x13e7a41d, 0xbad4, 0x4ab2, {0x80, 0x6b, 0xe5, 0x71, 0x28, 0x30, 0x26, 0x84} };
